91dd0616a785eee32d2e40b3b841b546ec77cecb
Mark A. Gibbs <indi.in.the.wired@gmail.com>
2015-06-05 08:03:42 -0400

    Better comments for detail/write

--- include/boost/rangeio/detail/write.hpp
+++ include/boost/rangeio/detail/write.hpp
@@ -5,6 +5,11 @@
 // (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 // 
 
+// Contains low-level implementation functions for the range write operations
+// used throughout the library.
+// 
+// This file is written to be C++98-safe.
+
 #ifndef BOOST_RANGEIO_Inc_detail_X_write_all_2015_01_01_
 #define BOOST_RANGEIO_Inc_detail_X_write_all_2015_01_01_
 
@@ -18,12 +23,33 @@ namespace boost {
 namespace rangeio {
 namespace detail {
 
-template <typename InputIterator, typename Sentinel, typename CharT, typename Traits>
+// Underlying implementation function for all versions of write without
+// delimiters.
+// 
+// Thus function is ultimately used under the hood by all of the write
+// functions in the RangeIO library that do not write delimiters between the
+// elements. The interface is designed so that higher level structures can
+// have their members updated via references as the write proceeds, so if there
+// is any kind of error, those members will be left in the last good state.
+// 
+// While i is not equal to e and bool(out) is true, performs "out << *i". If
+// boo(out) is still true, performs "++i" and "++n".
+// 
+// Between each write, the stream's formatting state is restored to what it
+// was before the first write.
+// 
+// At the end of the function, whether there have been any writes or not, the
+// stream width is set to zero.
+template <
+  typename InputIterator,
+  typename Sentinel,
+  typename CharT,
+  typename Traits>
 void
 write_impl(
   ::std::basic_ostream<CharT, Traits>& out,
   InputIterator& i,
-  Sentinel e,
+  Sentinel const e,
   ::std::size_t& n)
 {
   // Only bother to attempt writing if the range is empty (i == e) or the
@@ -36,8 +62,8 @@ write_impl(
     if ((out << *i))
     {
       // If the first write succeeds, increment:
-      ++n; // ... the iterator
-      ++i; // ... the write count
+      ++n; // ... the write count (do first because it will never throw)
+      ++i; // ... the iterator
       
       while ((i != e) && bool(out))
       {
@@ -49,8 +75,8 @@ write_impl(
         if ((out << *i))
         {
           // If the next write succeeds, increment:
-          ++n; // ... the iterator
-          ++i; // ... the write count
+          ++n; // ... the write count (do first because it will never throw)
+          ++i; // ... the iterator
         }
       }
     }
@@ -60,12 +86,34 @@ write_impl(
   out.width(0);
 }
 
-template <typename InputIterator, typename Sentinel, typename Delimiter, typename CharT, typename Traits>
+// Underlying implementation function for all versions of write with delimiters.
+// 
+// Thus function is ultimately used under the hood by all of the write
+// functions in the RangeIO library that write delimiters between the elements.
+// The interface is designed so that higher level structures can have their
+// members updated via references as the write proceeds, so if there is any
+// kind of error, those members will be left in the last good state.
+// 
+// While i is not equal to e and bool(out) is true, performs "out << *i". If
+// boo(out) is still true, performs "++i" and "++n".
+// 
+// Between each write, "out << delim" is performed. If bool(out) is still true
+// after that, the stream's formatting state is restored to what it was before
+// the first write.
+// 
+// At the end of the function, whether there have been any writes or not, the
+// stream width is set to zero.
+template <
+  typename InputIterator,
+  typename Sentinel,
+  typename Delimiter,
+  typename CharT,
+  typename Traits>
 void
 write_impl(
   ::std::basic_ostream<CharT, Traits>& out,
   InputIterator& i,
-  Sentinel e,
+  Sentinel const e,
   Delimiter& delim,
   ::std::size_t& n)
 {
@@ -79,8 +127,8 @@ write_impl(
     if ((out << *i))
     {
       // If the first write succeeds, increment:
-      ++n; // ... the iterator
-      ++i; // ... the write count
+      ++n; // ... the write count (do first because it will never throw)
+      ++i; // ... the iterator
       
       while ((i != e) && bool(out) && (out << delim))
       {
@@ -92,8 +140,8 @@ write_impl(
         if ((out << *i))
         {
           // If the next write succeeds, increment:
-          ++n; // ... the iterator
-          ++i; // ... the write count
+          ++n; // ... the write count (do first because it will never throw)
+          ++i; // ... the iterator
         }
       }
     }
